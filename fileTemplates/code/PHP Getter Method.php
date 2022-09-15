7ed6a078f88436b1df0859a610d3b3e10390465f
Yannick Voyer <yvoyer@users.noreply.github.com>
2022-09-15 15:22:49 -0400

    Getter now return type hint

--- fileTemplates/code/PHP Getter Method.php
+++ fileTemplates/code/PHP Getter Method.php
@@ -1,9 +1,6 @@
 #set($propertyName = $NAME.substring(0,1).toLowerCase().concat($NAME.substring(1)))
 
-/**
-* @return ${TYPE_HINT}
-*/
-public ${STATIC} function $propertyName()
+public ${STATIC} function $propertyName(): ${TYPE_HINT}
 {
 #if (${STATIC} == "static")
 return self::$${FIELD_NAME};
