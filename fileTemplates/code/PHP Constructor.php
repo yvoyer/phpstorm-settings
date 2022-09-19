1737ec79515ef53ea277e6e2a3ddb59ef5a8a958
Yannick Voyer <yvoyer@users.noreply.github.com>
2022-09-19 16:43:07 -0400

    Allow args beginning with "_" when < 3 args

--- fileTemplates/code/PHP Constructor.php
+++ fileTemplates/code/PHP Constructor.php
@@ -1,8 +1,8 @@
 #set($parameters = $PARAM_LIST)
 
 #if ($parameters.split(',').size() > 3)
-#set($parameters = $PARAM_LIST.replace('$_', '$').replace(',', ",
+#set($parameters = $PARAM_LIST.replace(',', ",
 "))
 #end
 #set($body = ${BODY})
-public function __construct($parameters) {$body.replace('$_', '$')}
+public function __construct($parameters.replace('$_', '$')) {$body.replace('$_', '$')}
